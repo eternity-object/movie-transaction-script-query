c8267a0b7892f7a71e601e157dc8d759c0288597
eternity-object <eternity.object@gmail.com>
2025-01-15 13:24:19 +0900

    프로젝트 초기화

---
+++ src/test/java/org/eternity/script/movie/jpa/JpaPersistenceContextTest.java
@@ -0,0 +1,47 @@
+package org.eternity.script.movie.jpa;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest(showSql = false)
+public class JpaPersistenceContextTest {
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    public void sequence_transactional_write_behind() {
+
+    }
+
+    @Test
+    public void identity_transactional_write_behind() {
+
+    }
+
+    @Test
+    public void first_level_cache() {
+
+    }
+
+    @Test
+    public void remove() {
+
+    }
+
+    @Test
+    public void automatic_dirty_checking() {
+
+    }
+
+    @Test
+    public void detach_and_merge() {
+
+    }
+
+    @Test
+    public void query_and_flush() {
+
+    }
+}
